b4a70e64264bf701ac8e2d9a4075045afcd164d3
youchief <cyril@youchief.ch>
2017-09-20 17:07:05 +0200

    new import version

--- src/Controller/Admin/ContactsController.php
+++ src/Controller/Admin/ContactsController.php
@@ -3,13 +3,14 @@ namespace Trois\Newsletter\Controller\Admin;
 
 use Trois\Newsletter\Controller\AppController;
 
+
 /**
- * Contacts Controller
- *
- * @property \Trois\Newsletter\Model\Table\ContactsTable $Contacts
- *
- * @method \Trois\Newsletter\Model\Entity\Contact[] paginate($object = null, array $settings = [])
- */
+* Contacts Controller
+*
+* @property \Trois\Newsletter\Model\Table\ContactsTable $Contacts
+*
+* @method \Trois\Newsletter\Model\Entity\Contact[] paginate($object = null, array $settings = [])
+*/
 class ContactsController extends AppController
 {
 
@@ -32,24 +33,28 @@ class ContactsController extends AppController
         }else{
           $contacts = [];
           $check = [];
-          foreach($matches[0] as $match)
-          {
+          foreach($matches[0] as $match){
             if(!empty($check[$match]))
-              continue;
+            continue;
             $check[$match] = true;
-            array_push($contacts,[
-              'email' => $match,
-              'mailing_list_id' => $this->request->data['mailing_list_id']
-            ]);
+            if(filter_var($match, FILTER_VALIDATE_EMAIL)) {
+              $cont = $this->Contacts->newEntity();
+              $cont->email = $match;
+              $cont->mailing_list_id = $this->request->data['mailing_list_id'];
+              $contacts[] = $cont;
+            }
           }
-          $entities = $this->Contacts->newEntities($contacts);
-          $result = $this->Contacts->saveMany($entities);
-          if (!empty($result)) {
-            $this->Flash->success(count($entities).' '.__('contacts have been saved.'));
-            return $this->redirect(['action' => 'index']);
-          }else{
-            $this->Flash->error(__('The contacts could not be saved. Please, try again.'));
+          $saved = 0;
+          $error = 0;
+          foreach ($contacts as $new) {
+            if ($this->Contacts->save($new)) {
+              $saved++;
+            }else{
+              $error++;
+            }
           }
+          $this->Flash->success($saved." email was imported");
+          $this->Flash->error($error." email was not imported");
         }
       }
     }
@@ -58,104 +63,104 @@ class ContactsController extends AppController
     $this->set('_serialize', ['contact']);
   }
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['MailingLists']
-        ];
-        $contacts = $this->paginate($this->Contacts);
+  /**
+  * Index method
+  *
+  * @return \Cake\Http\Response|void
+  */
+  public function index()
+  {
+    $this->paginate = [
+      'contain' => ['MailingLists']
+    ];
+    $contacts = $this->paginate($this->Contacts);
 
-        $this->set(compact('contacts'));
-        $this->set('_serialize', ['contacts']);
-    }
+    $this->set(compact('contacts'));
+    $this->set('_serialize', ['contacts']);
+  }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Contact id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $contact = $this->Contacts->get($id, [
-            'contain' => ['MailingLists']
-        ]);
+  /**
+  * View method
+  *
+  * @param string|null $id Contact id.
+  * @return \Cake\Http\Response|void
+  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+  */
+  public function view($id = null)
+  {
+    $contact = $this->Contacts->get($id, [
+      'contain' => ['MailingLists']
+    ]);
 
-        $this->set('contact', $contact);
-        $this->set('_serialize', ['contact']);
-    }
+    $this->set('contact', $contact);
+    $this->set('_serialize', ['contact']);
+  }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $contact = $this->Contacts->newEntity();
-        if ($this->request->is('post')) {
-            $contact = $this->Contacts->patchEntity($contact, $this->request->getData());
-            if ($this->Contacts->save($contact)) {
-                $this->Flash->success(__('The contact has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The contact could not be saved. Please, try again.'));
-        }
-        $mailingLists = $this->Contacts->MailingLists->find('list', ['limit' => 200]);
-        $this->set(compact('contact', 'mailingLists'));
-        $this->set('_serialize', ['contact']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Contact id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $contact = $this->Contacts->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $contact = $this->Contacts->patchEntity($contact, $this->request->getData());
-            if ($this->Contacts->save($contact)) {
-                $this->Flash->success(__('The contact has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The contact could not be saved. Please, try again.'));
-        }
-        $mailingLists = $this->Contacts->MailingLists->find('list', ['limit' => 200]);
-        $this->set(compact('contact', 'mailingLists'));
-        $this->set('_serialize', ['contact']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Contact id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $contact = $this->Contacts->get($id);
-        if ($this->Contacts->delete($contact)) {
-            $this->Flash->success(__('The contact has been deleted.'));
-        } else {
-            $this->Flash->error(__('The contact could not be deleted. Please, try again.'));
-        }
+  /**
+  * Add method
+  *
+  * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+  */
+  public function add()
+  {
+    $contact = $this->Contacts->newEntity();
+    if ($this->request->is('post')) {
+      $contact = $this->Contacts->patchEntity($contact, $this->request->getData());
+      if ($this->Contacts->save($contact)) {
+        $this->Flash->success(__('The contact has been saved.'));
 
         return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('The contact could not be saved. Please, try again.'));
     }
+    $mailingLists = $this->Contacts->MailingLists->find('list', ['limit' => 200]);
+    $this->set(compact('contact', 'mailingLists'));
+    $this->set('_serialize', ['contact']);
+  }
+
+  /**
+  * Edit method
+  *
+  * @param string|null $id Contact id.
+  * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+  * @throws \Cake\Network\Exception\NotFoundException When record not found.
+  */
+  public function edit($id = null)
+  {
+    $contact = $this->Contacts->get($id, [
+      'contain' => []
+    ]);
+    if ($this->request->is(['patch', 'post', 'put'])) {
+      $contact = $this->Contacts->patchEntity($contact, $this->request->getData());
+      if ($this->Contacts->save($contact)) {
+        $this->Flash->success(__('The contact has been saved.'));
+
+        return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('The contact could not be saved. Please, try again.'));
+    }
+    $mailingLists = $this->Contacts->MailingLists->find('list', ['limit' => 200]);
+    $this->set(compact('contact', 'mailingLists'));
+    $this->set('_serialize', ['contact']);
+  }
+
+  /**
+  * Delete method
+  *
+  * @param string|null $id Contact id.
+  * @return \Cake\Http\Response|null Redirects to index.
+  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+  */
+  public function delete($id = null)
+  {
+    $this->request->allowMethod(['post', 'delete']);
+    $contact = $this->Contacts->get($id);
+    if ($this->Contacts->delete($contact)) {
+      $this->Flash->success(__('The contact has been deleted.'));
+    } else {
+      $this->Flash->error(__('The contact could not be deleted. Please, try again.'));
+    }
+
+    return $this->redirect(['action' => 'index']);
+  }
 }
--- src/Model/Table/ContactsTable.php
+++ src/Model/Table/ContactsTable.php
@@ -7,82 +7,98 @@ use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
 /**
- * Contacts Model
- *
- * @property \Trois\Newsletter\Model\Table\MailingListsTable|\Cake\ORM\Association\BelongsTo $MailingLists
- *
- * @method \Trois\Newsletter\Model\Entity\Contact get($primaryKey, $options = [])
- * @method \Trois\Newsletter\Model\Entity\Contact newEntity($data = null, array $options = [])
- * @method \Trois\Newsletter\Model\Entity\Contact[] newEntities(array $data, array $options = [])
- * @method \Trois\Newsletter\Model\Entity\Contact|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \Trois\Newsletter\Model\Entity\Contact patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method \Trois\Newsletter\Model\Entity\Contact[] patchEntities($entities, array $data, array $options = [])
- * @method \Trois\Newsletter\Model\Entity\Contact findOrCreate($search, callable $callback = null, $options = [])
- *
- * @mixin \Cake\ORM\Behavior\TimestampBehavior
- */
+* Contacts Model
+*
+* @property \Trois\Newsletter\Model\Table\MailingListsTable|\Cake\ORM\Association\BelongsTo $MailingLists
+*
+* @method \Trois\Newsletter\Model\Entity\Contact get($primaryKey, $options = [])
+* @method \Trois\Newsletter\Model\Entity\Contact newEntity($data = null, array $options = [])
+* @method \Trois\Newsletter\Model\Entity\Contact[] newEntities(array $data, array $options = [])
+* @method \Trois\Newsletter\Model\Entity\Contact|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+* @method \Trois\Newsletter\Model\Entity\Contact patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+* @method \Trois\Newsletter\Model\Entity\Contact[] patchEntities($entities, array $data, array $options = [])
+* @method \Trois\Newsletter\Model\Entity\Contact findOrCreate($search, callable $callback = null, $options = [])
+*
+* @mixin \Cake\ORM\Behavior\TimestampBehavior
+*/
 class ContactsTable extends Table
 {
 
-    /**
-     * Initialize method
-     *
-     * @param array $config The configuration for the Table.
-     * @return void
-     */
-    public function initialize(array $config)
-    {
-        parent::initialize($config);
+  /**
+  * Initialize method
+  *
+  * @param array $config The configuration for the Table.
+  * @return void
+  */
+  public function initialize(array $config)
+  {
+    parent::initialize($config);
 
-        $this->setTable('contacts');
-        $this->setDisplayField('id');
-        $this->setPrimaryKey('id');
+    $this->setTable('contacts');
+    $this->setDisplayField('id');
+    $this->setPrimaryKey('id');
 
-        $this->addBehavior('Timestamp');
+    $this->addBehavior('Timestamp');
 
-        $this->belongsTo('MailingLists', [
-            'foreignKey' => 'mailing_list_id',
-            'joinType' => 'INNER',
-            'className' => 'Trois/Newsletter.MailingLists'
-        ]);
-    }
+    $this->belongsTo('MailingLists', [
+      'foreignKey' => 'mailing_list_id',
+      'joinType' => 'INNER',
+      'className' => 'Trois/Newsletter.MailingLists'
+    ]);
+  }
 
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
-    public function validationDefault(Validator $validator)
-    {
-        $validator
-            ->integer('id')
-            ->allowEmpty('id', 'create');
+  /**
+  * Default validation rules.
+  *
+  * @param \Cake\Validation\Validator $validator Validator instance.
+  * @return \Cake\Validation\Validator
+  */
+  public function validationDefault(Validator $validator)
+  {
+    $validator
+    ->integer('id')
+    ->allowEmpty('id', 'create');
 
-        $validator
-            ->email('email')
-            ->requirePresence('email', 'create')
-            ->notEmpty('email');
+    $validator
+    ->email('email')
+    ->requirePresence('email', 'create')
+    ->notEmpty('email')
+    ->add('email',['unique' => [
+      'rule' => 'validateUnique',
+      'provider' => 'table',
+      'message' => __('Not unique')
+      ]]
+    );
 
-        $validator
-            ->boolean('subscribe')
-            ->allowEmpty('subscribe');
+    /*$validator->add(
+      'email',
+      ['unique' => [
+        'rule' => 'validateUnique',
+        'provider' => 'table',
+        'message' => 'Not unique']
+      ]
+    );*/
 
-        return $validator;
-    }
 
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
-    public function buildRules(RulesChecker $rules)
-    {
-        $rules->add($rules->isUnique(['email']));
-        $rules->add($rules->existsIn(['mailing_list_id'], 'MailingLists'));
+    $validator
+    ->boolean('subscribe')
+    ->allowEmpty('subscribe');
 
-        return $rules;
-    }
+    return $validator;
+  }
+
+  /**
+  * Returns a rules checker object that will be used for validating
+  * application integrity.
+  *
+  * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+  * @return \Cake\ORM\RulesChecker
+  */
+  public function buildRules(RulesChecker $rules)
+  {
+    $rules->add($rules->isUnique(['email']));
+    $rules->add($rules->existsIn(['mailing_list_id'], 'MailingLists'));
+
+    return $rules;
+  }
 }
